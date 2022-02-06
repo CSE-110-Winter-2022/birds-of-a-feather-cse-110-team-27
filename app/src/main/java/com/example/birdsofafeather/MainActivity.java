3ed5b739dd6a3278d0620106c92f4247da0f3f0d
Emil <esharkov@ucsd.edu>
2022-02-05 20:28:35 -0800

    bluetooth permissions working + generics >:)

--- app/src/main/java/com/example/birdsofafeather/MainActivity.java
+++ app/src/main/java/com/example/birdsofafeather/MainActivity.java
@@ -1,34 +1,13 @@
 package com.example.birdsofafeather;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.annotation.SuppressLint;
-import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
     @Override
     protected void onStart() {
         super.onStart();
-        requestEnableBluetooth();
+        Bluetooth.requestEnableBluetooth(this);
     }
-
-    public boolean bluetoothEnabled(){
-        if (bluetoothAdapter == null) {
-            return false;
-        }
-        return bluetoothAdapter.isEnabled();
-    }
-
-    @SuppressLint("MissingPermission")
-    public void requestEnableBluetooth(){
-        if(!bluetoothEnabled()){
-            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(intent);
-        }
-    }
-
 }
