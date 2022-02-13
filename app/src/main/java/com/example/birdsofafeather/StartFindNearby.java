6a9c147bfe975aba3b65cac25fcf18b7a6aa0088
Emil <esharkov@ucsd.edu>
2022-02-12 20:25:24 -0800

    start mocking Service/ no crashing / no constant printing in terminal

--- app/src/main/java/com/example/birdsofafeather/StartFindNearby.java
+++ app/src/main/java/com/example/birdsofafeather/StartFindNearby.java
@@ -19,9 +19,9 @@ import java.util.List;
 public class StartFindNearby extends AppCompatActivity {
     private Button start;
     private Button stop;
-    private MessageListener messageListener;
-    private static final String TAG = "FindNearby";
-    public String nearbyMessage;
+    public static MessageListener messageListener;
+    public static final String TAG = "FindNearby";
+    public static String nearbyMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,19 +35,7 @@ public class StartFindNearby extends AppCompatActivity {
         MockUserWithCourses Zoey = new MockUserWithCourses(2);
         nearbyMessage = John + "\n" + Amy + "\n" + Zoey;
 
-        MessageListener realListener = new MessageListener() {
-            @Override
-            public void onFound(@NonNull Message message) {
-                Log.d(TAG, "Found message: " + new String(message.getContent()));
-            }
 
-            @Override
-            public void onLost(@NonNull Message message) {
-                Log.d(TAG, "Lost sign of message: " + new String(message.getContent()));
-            }
-        };
-
-        this.messageListener = new FakedMessageListener(realListener, 10, nearbyMessage);
     }
 
     public void startClicked(View view){
@@ -62,18 +50,17 @@ public class StartFindNearby extends AppCompatActivity {
         stop.setVisibility(View.GONE);
         start.setVisibility(View.VISIBLE);
         stopService(intent);
-        Nearby.getMessagesClient(this).unsubscribe(messageListener);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Nearby.getMessagesClient(this).subscribe(messageListener);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Nearby.getMessagesClient(this).unsubscribe(messageListener);
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Nearby.getMessagesClient(this).subscribe(messageListener);
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Nearby.getMessagesClient(this).unsubscribe(messageListener);
+//    }
 }
