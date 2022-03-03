ea6223a9d4f043b9fffd660589c7b24a899dc5eb
Byron Chan <bschan@UCSD.EDU>
2022-03-02 19:57:07 -0800

    Updated Example Unit Test

--- app/src/test/java/com/example/birdsofafeather/ExampleUnitTest.java
+++ app/src/test/java/com/example/birdsofafeather/ExampleUnitTest.java
@@ -21,30 +21,5 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
-
-    @Test
-    public void courseArrayMakerTest() {
-        List<Course> listOfCourses = new ArrayList<Course>();
-
-        // Initializes 3 different courses
-        Course course0 = new Course(0, 0, 2021, "Fall", "ECE", 101, "SMALL");
-        Course course1 = new Course(0, 0, 2022, "Winter", "CSE", 110, "SMALL");
-        Course course2 = new Course(0, 0, 2022, "Winter", "CSE", 101, "SMALL");
-
-        listOfCourses.add(course0);
-        listOfCourses.add(course1);
-        listOfCourses.add(course2);
-
-        ArrayList<String> user = courseArrayMaker(listOfCourses, "Matt");
-
-        String courseIndex0 = "ECE 101 2021 Fall";
-        String courseIndex1 = "CSE 110 2022 Winter";
-        String courseIndex2 = "CSE 101 2022 Winter";
-
-        assertEquals(courseIndex0, user.get(0));
-        assertEquals(courseIndex1, user.get(1));
-        assertEquals(courseIndex2, user.get(2));
-
-    }
 }
 
