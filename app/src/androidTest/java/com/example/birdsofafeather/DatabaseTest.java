99006ee4903e606659c1bf123508768d29a32bda
Yufei Weng <y8weng@ucsd.edu>
2022-03-11 17:04:26 -0800

    Two tests with session

--- app/src/androidTest/java/com/example/birdsofafeather/DatabaseTest.java
+++ app/src/androidTest/java/com/example/birdsofafeather/DatabaseTest.java
@@ -12,11 +12,13 @@ import static org.junit.Assert.*;
 
 import com.example.birdsofafeather.db.AppDatabase;
 import com.example.birdsofafeather.db.course.Course;
+import com.example.birdsofafeather.db.session.Session;
 import com.example.birdsofafeather.db.user.User;
 import com.example.birdsofafeather.db.user.UserWithCourses;
 import com.example.birdsofafeather.generator.Generator;
 import com.example.birdsofafeather.generator.WaveCSVGenerator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -119,5 +121,48 @@ public class DatabaseTest {
         db.coursesDao().delete(classCancelled);
         UserWithCourses studentData = db.userWithCoursesDao().getUser(studentid);
         assertEquals(0,studentData.getCourses().size());
+    }
+
+    @Test
+    public void checkUserWSession(){
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.birdsofafeather", appContext.getPackageName());
+
+        AppDatabase db = AppDatabase.singleton(appContext);
+        User Gary = new User("Gary", "GG@ucsd.edu", "");
+        Gary.setSessionId(9527);
+        assertEquals(9527, Gary.getSessionId());
+    }
+    @Test
+
+    public void checkSessionWUsers() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.birdsofafeather", appContext.getPackageName());
+
+        AppDatabase db = AppDatabase.singleton(appContext);
+        User A = new User("A", "a@ucsd.edu", "");
+        long Aid = db.userWithCoursesDao().insert(A);
+        User B = new User("B", "b@ucsd.edu", "");
+        long Bid = db.userWithCoursesDao().insert(B);
+        User C = new User("C", "b@ucsd.edu", "");
+        long Cid = db.userWithCoursesDao().insert(C);
+
+        UserWithCourses Adata = db.userWithCoursesDao().getUser(Aid);
+        UserWithCourses Bdata = db.userWithCoursesDao().getUser(Bid);
+        UserWithCourses Cdata = db.userWithCoursesDao().getUser(Cid);
+
+        Session session = new Session("session");
+        long sessionid = db.sessionWithUsersDao().insertSession(session);
+        List<UserWithCourses> listOfUsers = new ArrayList<UserWithCourses>();
+        listOfUsers.add(Adata);
+        listOfUsers.add(Bdata);
+        listOfUsers.add(Cdata);
+
+        db.sessionWithUsersDao().addUsersToSession(sessionid, listOfUsers);
+        List<User> users = db.sessionWithUsersDao().getUsersForSessionId(sessionid);
+        System.out.println(users.toString());
+        assertEquals("A", users.get(0).getName());
+        assertEquals("B", users.get(1).getName());
+        assertEquals("C", users.get(2).getName());
     }
 }
